931ff5450951388dc7d0ae14ffa3a9285162d3fb
Yitao Xiong <yitao@bitsighttech.com>
2018-03-29 16:12:43 -0400

    加入了Session和Basic Auth，补充了序列器的测试

--- config/settings/common.py
+++ config/settings/common.py
@@ -154,6 +154,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
     ),
 }
 
