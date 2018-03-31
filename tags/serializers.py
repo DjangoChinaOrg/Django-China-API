03fa0615d223a821d6d8c1592c6af0e1d899e30d
Aaron Zhao <rudy710@qq.com>
2018-03-31 13:09:19 +0800

    完善了postSerializer返回的数据

--- tags/serializers.py
+++ tags/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Tag
 
 
-class TagSerializer(serializers.ModelSerializer):
+class TagSerializer(serializers.HyperlinkedModelSerializer):
     creator = serializers.ReadOnlyField(source='creator.username')
 
     class Meta:
