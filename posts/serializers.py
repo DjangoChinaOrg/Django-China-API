d236fbafb799a66bde164fe283752cef05fb2b24
Aaron Zhao <rudy710@qq.com>
2018-03-31 13:09:19 +0800

    完善了postSerializer返回的数据

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,17 +1,83 @@
 from rest_framework import serializers
+from django.contrib.contenttypes.models import ContentType
 
 from .models import Post
+from tags.serializers import TagSerializer
+from replies.api.serializers import TreeReplySerializer
 
 
-class PostSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    帖子序列化器，用于显示帖子列表，帖子详情
+    以及帖子创建、编辑
+    """
+    # TODO: 等待userserializer的定义，返回更相信的author信息
     author = serializers.ReadOnlyField(source='author.username')
+
+    replies = serializers.SerializerMethodField()
+    reply_count = serializers.SerializerMethodField()
+    participants_count = serializers.SerializerMethodField()
+    content_type = serializers.SerializerMethodField()
+    tags = TagSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
-        exclude = ('hidden',)
+        read_only_fields = (
+            'id',
+            'content_type',
+            'url',
+            'views',
+            'created_time',
+            'modified_time',
+            'pinned',
+            'highlighted',
+            'replies',
+            'reply_count',
+            'participants_count',
+        )
+        fields = (
+            'id',
+            'content_type',
+            'url',
+            'title',
+            'body',
+            'views',
+            'created_time',
+            'modified_time',
+            'pinned',
+            'highlighted',
+            'tags',
+            'author',
+            'reply_count',
+            'participants_count',
+            'replies',
+        )
 
-    # 验证tag field
-    def validate_tags(self, value):
-        if len(value) > 3:
-            raise serializers.ValidationError("最多可以选择3个标签")
-        return value
+    def get_content_type(self, value):
+        content_type = ContentType.objects.get_for_model(value)
+        return content_type.id
+
+    def get_replies(self, value):
+        """
+        返回帖子下的回复
+        """
+        replies = value.replies
+        serializer = TreeReplySerializer(replies, many=True)
+        return serializer.data
+
+    def get_reply_count(self, value):
+        """
+        返回帖子的回复数量
+        """
+        return value.replies.count()
+
+    def get_participants_count(self, value):
+        """
+        返回评论参与者数量
+        """
+        user_list = []
+        replies = value.replies.all()
+        for reply in replies:
+            if reply.user not in user_list:
+                user_list.append(reply.user)
+        return len(user_list)
--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,30 @@
 import datetime
+from collections import OrderedDict
 
 from django.utils.timezone import now
 from django.db.models import Max, Count
 
 from django_filters import rest_framework as filters
-from rest_framework import permissions, viewsets, pagination
+from rest_framework import permissions, viewsets, pagination, serializers
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
 
 from .models import Post
+from tags.models import Tag
 from .serializers import PostSerializer
 from .permissions import IsAdminAuthorOrReadOnly
 
 
 class PostPagination(pagination.PageNumberPagination):
     page_size = 20
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('count', self.page.paginator.count),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('posts', data)
+        ]))
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -25,16 +35,26 @@ class PostViewSet(viewsets.ModelViewSet):
                           IsAdminAuthorOrReadOnly)
     pagination_class = PostPagination
     # 允许get post put方法
-    http_method_names = ['get', 'post', 'put']
+    http_method_names = ['get', 'post', 'put', 'patch']
     filter_backends = (filters.DjangoFilterBackend,)
-    #在post-list页面可以按标签字段过滤出特定标签下的帖子
+    # 在post-list页面可以按标签字段过滤出特定标签下的帖子
     filter_fields = ('tags',)
 
     def perform_create(self, serializer):
         """
-        因为author字段在PostSerializer里是ReadOnly，所以这里需要手动保存
+        保存tags和author，同时验证tag的数量
+        tags和author在PostSerializer里是read_only
         """
-        serializer.save(author=self.request.user)
+        tags = []
+        tags_data = self.request.data.get('tags')
+        if len(tags_data) == 0:
+            raise serializers.ValidationError("请至少选择一个标签")
+        if len(tags_data) > 3:
+            raise serializers.ValidationError("最多可以选择3个标签")
+        for name in tags_data:
+            tag = Tag.objects.get(name=name)
+            tags.append(tag)
+        serializer.save(author=self.request.user, tags=tags)
 
     @list_route()
     def popular_posts(self, request):
