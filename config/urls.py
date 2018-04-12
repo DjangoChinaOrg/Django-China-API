d999008cd2908de5b81a2a95e8b2b640e73f8bff
yangxg <zmrenwu@163.com>
2018-04-12 18:06:45 +0800

    add docs and required packages

--- config/urls.py
+++ config/urls.py
@@ -18,14 +18,15 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from rest_framework.documentation import include_docs_urls
 from rest_framework.routers import DefaultRouter
 from rest_framework_jwt.views import refresh_jwt_token
 
+from notification_extension.views import NotificationViewSet
 from posts.views import PostViewSet
 from replies.api.views import ReplyViewSet
 from tags.views import TagViewSet
 from users.views import EmailAddressViewSet, LoginViewCustom, UserViewSets
-from notification_extension.views import NotificationViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -42,4 +43,5 @@ urlpatterns = [
     url(r'^rest-auth/jwt-refresh/', refresh_jwt_token),
     url(r'^api-auth/', include('rest_framework.urls')),  # 仅仅用于测试
     url(r'^', include(router.urls)),
+    url(r'^docs/', include_docs_urls(title='Django中文社区 API'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
